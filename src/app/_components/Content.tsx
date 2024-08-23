7aa287c1a919f7c8d390c4a8b987b9e96f62553d
yoannBonge <yoannbonge@live.fr>
2024-08-23 11:34:00 +0200

    Fix h2 projects display issue

--- src/app/_components/Content.tsx
+++ src/app/_components/Content.tsx
@@ -61,7 +61,7 @@ export default function Content() {
         className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
         aria-label="Mes projets"
       >
-        <div className="sticky top-0 -mx-6 mb-4 w-screen bg-gray-800/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:relative lg:top-auto lg:mx-auto lg:mb-12 lg:w-full lg:bg-transparent lg:px-0 lg:py-0">
+        <div className="sticky top-0 -mx-6 mb-4 w-screen bg-gray-800/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:relative lg:top-auto lg:z-10 lg:mx-auto lg:mb-12 lg:w-full lg:bg-transparent lg:px-0 lg:py-0">
           <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:text-xl">
             Mes projets
           </h2>
